9454291394f9a9481d61c23d2f4a3fac98bf01cd
achao <anthony.r.chao@gmail.com>
2019-02-18 17:27:48 -0800

    algorithmic toolbox week2: completed fast fib with memoization

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,8 +1,14 @@
 # Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
+memoize = {
+        0 : 1,
+        1 : 1,
+        }
 
+def calc_fib(n):
+    if n in memoize:
+        return memoize[n]
+    else:
+        memoize[n] = calc_fib(n - 1) + calc_fib(n - 2)
     return calc_fib(n - 1) + calc_fib(n - 2)
 
 n = int(input())
