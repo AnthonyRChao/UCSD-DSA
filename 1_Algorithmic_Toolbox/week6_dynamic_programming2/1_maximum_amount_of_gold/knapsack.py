aad7c43ad88e0128f240e2aece9079db42e17e81
achao <anthony.r.chao@gmail.com>
2019-03-24 23:24:46 -0700

    Algorithmic Toolbox: passed week 6, completed 2/3 assignments

--- 1_Algorithmic_Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ 1_Algorithmic_Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -2,12 +2,17 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    res = [[0 for i in range(W + 1)] for j in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for weight in range(1, W + 1):
+            res[i][weight] = res[i - 1][weight]
+            if w[i - 1] <= weight:
+                value = res[i - 1][weight - w[i - 1]] + w[i - 1]
+                if value > res[i][weight]:
+                    res[i][weight] = value
+    return res[n][W]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- 1_Algorithmic_Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ 1_Algorithmic_Toolbox/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,16 +1,37 @@
 # Uses python3
 import sys
-import itertools
+
+def partition(a, p, r):
+    pivot = p
+    for i in range(p + 1, r + 1):
+        if(a[i] < a[pivot]):
+            tmp = a[i]
+            a[i] = a[pivot + 1]
+            a[pivot] = tmp
+            pivot = pivot + 1
+    return pivot
+
+def quickSort(a, p, r):
+    if(r > p):
+        x = partition(a, p, r)
+        quickSort(a, p, x - 1)
+        quickSort(a, x + 1, r)
 
 def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
-
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
-
+    if(sum(A) % 3 == 0):
+        quickSort(A, 0, len(A) - 1)
+        s = [list(), list(), list()]
+        i = len(A) - 1
+        while(i > -1):
+            j = 0
+            state = False
+            while(not state and j < len(s)):
+                if(sum(s[j]) + A[i] <= sum(A) // 3):
+                    s[j].append(A[i])
+                    state = True
+                j = j + 1
+            i = i - 1
+        return int(sum(s[0]) == sum(s[1]) == sum(s[2]) == sum(A) // 3)
     return 0
 
 if __name__ == '__main__':
