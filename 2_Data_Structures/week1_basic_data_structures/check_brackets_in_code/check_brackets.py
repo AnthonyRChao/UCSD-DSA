757d20db5cdf637ff225b6623b146c8eae610833
Anthony Chao <anthony.r.chao@gmail.com>
2019-03-31 16:42:01 -0700

    Update check_brackets.py

--- 2_Data_Structures/week1_basic_data_structures/check_brackets_in_code/check_brackets.py
+++ 2_Data_Structures/week1_basic_data_structures/check_brackets_in_code/check_brackets.py
@@ -5,27 +5,28 @@ from collections import namedtuple
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+    stack = []
+    for i, char in enumerate(text):
+        val = Bracket(char, i + 1)
+        if val[0] in "([{":
+            stack.append(val)
+        else:
+            if val[0] in '}])':
+                top = stack.pop()
+                if ((top[0] == '[' and val[0] != ']') or
+                        (top[0] == '(' and val[0] != ')') or
+                        (top[0] == '{' and val[0] != '}')):
+                       return val[1]
+    return 'Success' if len(stack) == 0 else stack[-1][1]
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
     main()
+
