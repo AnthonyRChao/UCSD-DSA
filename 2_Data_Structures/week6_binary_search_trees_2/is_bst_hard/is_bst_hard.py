877d7f1d7f3d3d9d6a0c6cda2421ec130b64da3d
achao <anthony.r.chao@gmail.com>
2019-05-05 23:46:09 -0700

    Data Structures, week 6 assignments

--- 2_Data_Structures/week6_binary_search_trees_2/is_bst_hard/is_bst_hard.py
+++ 2_Data_Structures/week6_binary_search_trees_2/is_bst_hard/is_bst_hard.py
@@ -5,19 +5,24 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
-
+def IsBinarySearchTree(i, mn, mx):
+  if not i in tree:
+      return True
+  if tree[i][0] < mn or tree[i][0] > mx:
+      return False
+  return IsBinarySearchTree(tree[i][1], mn, tree[i][0] - 1)  and IsBinarySearchTree(tree[i][2], tree[i][0], mx)
 
 def main():
   nodes = int(sys.stdin.readline().strip())
-  tree = []
+  global tree
+  tree = {}
+  int_max = sys.maxsize
+  int_min = - sys.maxsize - 1
   for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
+    tree[i] = (list(map(int, sys.stdin.readline().strip().split())))
+  if IsBinarySearchTree(0, int_min, int_max):
     print("CORRECT")
   else:
     print("INCORRECT")
 
-threading.Thread(target=main).start()
+threading.Thread(target = main).start()
